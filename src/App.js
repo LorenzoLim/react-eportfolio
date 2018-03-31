9829327fc7aec9501db325e79df4eda621b52d90
Lorenzo Lim <renzo-lim@hotmail.com>
2018-03-31 18:43:52 +1000

    React Router Package Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
+        <div className="App">
 
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
