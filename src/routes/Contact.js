b4029d09a8748bc8d92bf5d7f25a4e79a4d36e4d
LorenzoLim <renzo-lim@hotmail.com>
2018-04-19 22:12:47 +1000

    Added About Contact and Portfolio Pages

---
+++ src/routes/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavBar from '../components/navbar';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default Home;
