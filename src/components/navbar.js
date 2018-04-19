e35b9588471447abfc6e19efc34d5b3beaec488b
LorenzoLim <renzo-lim@hotmail.com>
2018-04-19 23:19:49 +1000

    Added css file and using glamor

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-// import { css } from 'glamor';
+import { css } from 'glamor';
 
-// let s = css({
-//   background: 'black'
-// })
+let s = css({
+  background: 'RGBA(104,34,139,0.9)',
+  height: '60px',
+  position: 'fixed',
+  width: '100%'
+})
 
 class NavBar extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div {...s}>Hello</div>
     );
   }
 }
