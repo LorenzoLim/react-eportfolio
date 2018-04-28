faeaa120cb67f19cce890b024bb9b0418e15397c
LorenzoLim <renzo-lim@hotmail.com>
2018-04-28 21:50:56 +1000

    image background added and Logo added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import { css } from 'glamor';
+const logo = require('../images/L.png')
 
-let s = css({
+const s = css({
   background: 'RGBA(104,34,139,0.9)',
   height: '60px',
   position: 'fixed',
@@ -12,7 +13,12 @@ let s = css({
 class NavBar extends Component {
   render() {
     return (
-      <div {...s}>Hello</div>
+      <div {...s}>
+        <img alt="Landing Page" src={ logo } style={{width: '', height: ''}} />
+        <div>
+          Home Contact Portfolio About
+        </div>
+      </div>
     );
   }
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from '../components/navbar';
+const village = require('../images/village.jpg')
 
 class Home extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <img src="../images/village.jpg" alt="Village" />
+        <img alt="Landing Page" src={ village } style={{width: '100%', height: '800px'}} />
       </div>
     );
   }
