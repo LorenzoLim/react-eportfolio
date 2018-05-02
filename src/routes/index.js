7e0bd5792bc14e3b59ac3e759e2d4679acf6653c
LorenzoLim <renzo-lim@hotmail.com>
2018-05-02 23:50:21 +1000

    Linked paths to components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import About from './About';
+import Contact from './Contact';
 import Home from './Home';
+import Portfolio from './Portfolio';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/Contact" exact component={Home} />
-      <Route path="/Portfolio" exact component={Home} />
-      <Route path="/About" exact component={Home} />
+      <Route path="/Contact" exact component={Contact} />
+      <Route path="/Portfolio" exact component={Portfolio} />
+      <Route path="/About" exact component={About} />
     </Switch>
   </BrowserRouter>
 );
