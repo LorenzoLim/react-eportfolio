03459010e388762e5cceee7b806ef648934898f7
Lorenzo Lim <renzo-lim@hotmail.com>
2023-07-31 19:53:42 +1000

    Added description

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <H1>Minimum ASCII Delete Sum for Two Strings</H1>
+      <div>
+        Given two strings s1 and s2, return the lowest ASCII sum of deleted
+        characters to make two strings equal.
+      </div>
+      <div>
+        <div>Example 1: </div>
+        <span>
+          Input: s1 = "sea", s2 = "eat" Output: 231 Explanation: Deleting "s"
+          from "sea" adds the ASCII value of "s" (115) to the sum. Deleting "t"
+          from "eat" adds 116 to the sum. At the end, both strings are equal,
+          and 115 + 116 = 231 is the minimum sum possible to achieve this.
+        </span>
+        <div>Example 2:</div>
+        <span>
+          Input: s1 = "delete", s2 = "leet" Output: 403 Explanation: Deleting
+          "dee" from "delete" to turn the string into "let", adds 100[d] +
+          101[e] + 101[e] to the sum. Deleting "e" from "leet" adds 101[e] to
+          the sum. At the end, both strings are equal to "let", and the answer
+          is 100+101+101+101 = 403. If instead we turned both strings into "lee"
+          or "eet", we would get answers of 433 or 417, which are higher.
+        </span>
+      </div>
     </div>
   );
 }
