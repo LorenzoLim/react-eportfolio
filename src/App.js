e23211c94550ee1ef85c5e1a8f4c73730f4507bd
Lorenzo Lim <renzo-lim@hotmail.com>
2023-10-12 21:28:16 +1000

    fixed h1 tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <H1>Minimum ASCII Delete Sum for Two Strings</H1>
+      <h1>Minimum ASCII Delete Sum for Two Strings</h1>
       <div>
         Given two strings s1 and s2, return the lowest ASCII sum of deleted
         characters to make two strings equal.
