021297aa3a653289d4fe082c6c0c0410c04fc1cc
Lorenzo Lim <renzo-lim@hotmail.com>
2024-09-04 11:54:19 +1000

    Started on navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import { About, Contact, Home, Portfolio} from "./pages"
 
 function App() {
-
-  const exampleOne = () => {
-    return "Answer to example Two"
-  }
-
   return (
     <div>
-      <H2>Title</H2>
-      <Home />
-      <About />
-      <Contact />
-      <Portfolio />
+      
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+function Navbar() {
+  return (
+    <div
+      id="nav"
+      style={{
+        height: 70,
+        background: "transparent",
+        display: "block",
+        position: "fixed",
+        width: "100%",
+        zIndex: 5,
+        transition: "all ease .5s",
+      }}
+    >
+      <ul
+        id="navbar"
+        style={{
+          listStyleType: "none",
+          margin: 0,
+          padding: 0,
+          position: "absolute",
+        }}
+      >
+        <li>
+          <a href="about.html" class="link logo">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="portfolio.html" class="link logo">
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a href="contact.html" class="link logo">
+            Contact
+          </a>
+        </li>
+        <li>
+          <a href="index.html" class="link logo">
+            Home
+          </a>
+        </li>
+      </ul>
+      <p id="L">
+        <img src="favicon/L.png" />
+      </p>
+    </div>
+  );
+}
+
+export default Navbar;
