4912ec21d32b986cf29a7a4fbefd5838f36bddc7
Lorenzo Lim <renzo-lim@hotmail.com>
2024-09-10 21:42:05 +1000

    Added navbar

---
+++ src/UI/NavbarButton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const NavbarButton = styled.div`
+  height: 60x;
+  width: 80px;
+  padding: 20px;
+  color: white;
+  background-color: #bf4f74;
+  text-align: center;
+  font-family: "Raleway", sans-serif;
+  font-size: 25px;
+`;
+
+export default NavbarButton;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,9 @@
+import { NavbarButton } from "../UI";
+
 function Navbar() {
   return (
-    <div
-      id="nav"
-      style={{
-        height: 70,
-        background: "transparent",
-        backgroundColor: "lightblue",
-        display: "block",
-        position: "fixed",
-        zIndex: 3,
-        transition: "all ease .5s",
-      }}
-    >
-      <ul
-        id="navbar"
-        style={{
-          listStyleType: "none",
-          margin: 0,
-          padding: 0,
-          position: "absolute",
-        }}
-      >
-        <li>
-          <a href="about.html" class="link logo">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="portfolio.html" class="link logo">
-            Portfolio
-          </a>
-        </li>
-        <li>
-          <a href="contact.html" class="link logo">
-            Contact
-          </a>
-        </li>
-        <li>
-          <a href="index.html" class="link logo">
-            Home
-          </a>
-        </li>
-      </ul>
-      <p id="L">
-        <img src="favicon/L.png" />
-      </p>
+    <div>
+      <NavbarButton>Test</NavbarButton>
     </div>
   );
 }
