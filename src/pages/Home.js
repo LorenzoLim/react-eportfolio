7a027518433ea9bd928adcf8f83fd7a7f86dd3f3
Lorenzo Lim <renzo-lim@hotmail.com>
2024-11-19 20:46:17 +1000

    Added another break

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -457,6 +457,7 @@ function Home() {
               }}
             >
               <br />
+              <br />
               However with this solution, the prices are stored in the database
               and can be adjusted on the fly so the prices will always be
               correct and the ease of use of not having to calculate costs
